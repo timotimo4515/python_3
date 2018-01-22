8dc0e71ebb4bce4e7057566fddd633c4336cc0e8
shedaniel <34910653+shedaniel@users.noreply.github.com>
2018-01-23 00:18:34 +0800

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,35 @@
+#A basic print function
+print("Hello World!")
+
+#Varibles
+a = 20
+b = 50
+sum = a + b
+print("The sum of a and b is", sum)
+
+#Varibles Error
+age = 20
+age_text = "years old"
+#print(age, age_text)
+#It should be:
+print(str(age), age_text)
+
+#User Input + Capitalize
+input1 = input("Give me your name! ")
+input2 = input("Give me your gender! ")
+print("\nResults of "+ input1.capitalize() + "\n-----------------------------\nGender: " + input2.capitalize())
+
+#Title
+title = "timo happy shop"
+print("\n\nWelcome to", title.title())
+item1 = "bread $100"
+print(item1.capitalize())
+item2 = "apple $500"
+print(item2.capitalize())
+item3 = "poop $31230"
+print(item3.capitalize())
+item4 = "timo $1"
+print(item4.capitalize())
+
+#Ending
+print("\nThis is the end of day 1, hope you have a better day tomorrow!")
