d3548502473356b02e83f37ee982bd45c2a18136
shedaniel <34910653+shedaniel@users.noreply.github.com>
2018-01-23 09:55:02 +0000

    Update Lesson2.py

--- Lesson2.py
+++ Lesson2.py
@@ -1,1 +1,14 @@
-
+#A Function Enabled Calculater
+def start(count):
+  i = 0
+  a = 0
+  b = 1
+  while(i < count):
+    sum = a + b
+    print(str(sum))
+    a = b
+    b = sum
+    i += 1
+    
+#The activater of the function
+start(10)
