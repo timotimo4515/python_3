d9d11d038435b8becb8caaf6eec45be791f7da93
shedaniel <34910653+shedaniel@users.noreply.github.com>
2018-01-24 19:24:41 +0800

    Create Lesson3.2.py

---
+++ Lesson3.2.py
@@ -0,0 +1,17 @@
+#String Index
+s = "This is a test string!"
+s_length = ien(s) - 1
+
+print("There is " + str(s_length + 1) + " letters.")
+print("The 11th letter is " + str(s[10]))
+
+#For loop
+for letter in s:
+  print(s)
+
+for x in range(5):
+  print(x)
+
+#count() & find()
+print("This letter 'i' has appeared " + s.count("i"))
+print("This letter 'g' has appeared in letter " + s.find("g"))
