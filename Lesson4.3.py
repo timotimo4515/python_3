0569180f3b654021f0504b37736508e429e064db
shedaniel <34910653+shedaniel@users.noreply.github.com>
2018-01-25 17:51:37 +0800

    "in" for loop & split()

---
+++ Lesson4.3.py
@@ -0,0 +1,13 @@
+foods = ["Ice Cream", "Poop", "Milk", "Rice"]
+
+#for loop with in
+for food in foods:
+    print(food)
+print("")
+
+message = "I love Danielshe!"
+message_list = message.split()
+
+#Printing Everything in the message_list
+for x in message_list:
+    print(x)
